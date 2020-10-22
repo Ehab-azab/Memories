60d10e45446fdfc4b06888ef4edc2226525a8cd6
Ehab Azab <Ehab-Azab>
2020-10-22 16:50:00 +0200

    this commit for making intro showed one time only when user oppen app at the first time

--- app/src/main/java/com/example/memories/activities/splash.kt
+++ app/src/main/java/com/example/memories/activities/splash.kt
@@ -1,5 +1,6 @@
 package com.example.memories.activities
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -10,33 +11,28 @@ class splash : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Handler().postDelayed(Runnable {
-            startActivity(Intent(this@splash, Mains_Activity::class.java))
-            this@splash.finish()
-        }, 2000)
-
-
-        /*val sharedPreferences=getSharedPreferences("prefrence", Context.MODE_PRIVATE)
-        val firststart=sharedPreferences.getBoolean("firststart",true)
-        if (firststart){
+        val sharedPreferences = getSharedPreferences("prefrence", Context.MODE_PRIVATE)
+        val firststart = sharedPreferences.getBoolean("firststart", true)
+        if (firststart) {
             firsttime()
-        }else{
+        } else {
             Handler().postDelayed(Runnable {
                 startActivity(Intent(this@splash, Home::class.java))
                 this@splash.finish()
             }, 2000)
-        }*/
+        }
 
 
     }
-    /*fun firsttime(){
+
+    fun firsttime() {
         Handler().postDelayed(Runnable {
             startActivity(Intent(this@splash, Mains_Activity::class.java))
             this@splash.finish()
         }, 2000)
-        val sharedPreferences=getSharedPreferences("prefrence", Context.MODE_PRIVATE)
-        val editor =sharedPreferences.edit()
-        editor.putBoolean("firststart",false)
+        val sharedPreferences = getSharedPreferences("prefrence", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putBoolean("firststart", false)
         editor.apply()
-    }*/
+    }
 }
