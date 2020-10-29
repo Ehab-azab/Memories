fa63a5577235a6ce5a406b5221f5e13187ee608c
Ehab Azab <Ehab-Azab>
2020-10-29 19:29:54 +0200

    tafnesha kda

--- app/src/main/java/com/example/memories/activities/Home.kt
+++ app/src/main/java/com/example/memories/activities/Home.kt
@@ -1,22 +1,26 @@
 package com.example.memories.activities
 
-import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.content.Intent
+import android.graphics.drawable.ClipDrawable.HORIZONTAL
+import android.icu.lang.UCharacter
 import android.os.Bundle
 import androidx.appcompat.widget.Toolbar
 import androidx.core.content.ContextCompat
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.memories.R
+import com.example.memories.adapters.Home_RecyclerView_Adapter
 import com.example.memories.database.Note
+import com.example.memories.database.NotesDatabase
 import com.example.memories.fragments.HomeFragment
 import com.example.memories.fragments.imageFragment
 import kotlinx.android.synthetic.main.activity_home.*
+import java.util.ArrayList
 
-import kotlinx.android.synthetic.main.activity_home.*
 import java.util.jar.Manifest
 
 class Home : AppCompatActivity() {
@@ -42,12 +46,22 @@ class Home : AppCompatActivity() {
 
 
         }
+        val adapter = Home_RecyclerView_Adapter(
+            NotesDatabase.getInstance(this).notesDao().getAllNotes() as ArrayList<Note>
+        )
+        home_recyclerview.adapter = adapter
+        home_recyclerview.layoutManager =
+            GridLayoutManager(this, 2, GridLayoutManager.VERTICAL, false)
+        adapter.onitemclicked = object : Home_RecyclerView_Adapter.OnItemClickListener {
+            override fun OnItemClicked(position: Int, noteId: Int?) {
+                sendid_andIntent(noteId!!)
+            }
 
+        }
 
     }
 
-    fun sendid_andIntent() {
-        var itemId = 5
+    fun sendid_andIntent(itemId: Int) {
         val intent = Intent(this, HomeFragment::class.java)
         intent.putExtra("id", itemId)
         startActivity(intent)
